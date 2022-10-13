99946faeb4a0e4e60df5e54a1f222f11e5922c6a
dida nurdiansyah <dida.nurdiansyah@gmail.com>
2022-10-13 14:52:18 +0700

    fix

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,55 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/generate-segitiga', function (Request $request) {
+    $number = $request->input('number');
+    $validated = preg_replace( '/[^0-9]/', '', $number );
+
+    if($validated) {
+        $arr_number = str_split($number);
+        $n = 0;
+        for ($x = 0; $x < count($arr_number); $x++) {
+            echo $arr_number[$x];
+            for ($y = 0; $y <= $n; $y++) {
+                echo "0";
+            }
+            $n++;
+            echo "<br>";
+        }
+    } else {
+        echo "error input not number";
+    }
+});
+
+Route::get('/generate-ganjil', function (Request $request) {
+    $number = $request->input('number');
+    $validated = preg_replace( '/[^0-9]/', '', $number );
+
+    if($validated) {
+        for ($x = 1; $x <= $number; $x++) {
+            if($x % 2 == 1) {
+                echo "$x <br>";
+            }
+        }
+    } else {
+        echo "error input not number";
+    }
+});
+
+
+Route::get('/generate-prima', function (Request $request) {
+    $number = $request->input('number');
+    $validated = preg_replace( '/[^0-9]/', '', $number );
+
+    if($validated) {
+        for ($x = 1; $x <= $number; $x++) {
+            if($x % 2 == 1) {
+                echo "$x <br>";
+            }
+        }
+    } else {
+        echo "error input not number";
+    }
 });
